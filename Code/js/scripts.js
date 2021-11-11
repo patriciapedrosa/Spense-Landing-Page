3e0e8ecfca1af5d3b773edc48dd0a6c06d1666bc
patriciapedrosa <patricia.pedrosa.1996@hotmail.com>
2021-11-11 15:57:29 +0000

    Add files via upload

--- Code/js/scripts.js
+++ Code/js/scripts.js
@@ -1,1 +1,37 @@
+$(document).ready(function(){
 
+	$('.img').slick({
+	    dots: false,
+		infinite: false,
+		arrows:false,
+		slidesToShow: 8,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 5,
+		        slidesToScroll: 5,
+		        infinite: true,
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 5,
+		        slidesToScroll: 5,
+		        infinite: true,
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2,
+		        infinite: true,
+		      }
+		    }
+		]
+	  });
+
+
+});
